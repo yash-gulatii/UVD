cee322ed91f001b280e5d4c63a94a1513c6d91f2
Yash-Gulatii <83213178+Yash-Gulatii@users.noreply.github.com>
2021-06-11 20:58:36 +0530

    Update UVD.py

--- UVD.py
+++ UVD.py
@@ -1,9 +1,20 @@
-import pytube  
+# Import Pytube 
+import pytube  # pip install pytube
+
+# Import YouTube from PyTube
 from pytube import YouTube  
+
+# Getting URL from the User
 video_url = input("Enter The Url of Video You Want To Download")
+
+# Getting Video Using PyTube
 youtube = pytube.YouTube(video_url)  
 video = youtube.streams.first()  
-video.download('C:\Users\yashg\Downloads')
+
+# Downloading Video to Downloading Folder( Remember to Change the Path Here :) )
+video.download('C:\Users\YourUser\Downloads')
+
+# Print the Info About Dowloading Status
 print("Downloding....")   
 print("Downloding...")   
 print("Downloding....")   
